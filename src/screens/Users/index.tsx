cb3136bd6dd755dea9bb38938c484596594b76dc
Fathah KA <48355244+fathah@users.noreply.github.com>
2024-12-16 17:19:52 +0400

    Package

--- src/screens/Users/index.tsx
+++ src/screens/Users/index.tsx
@@ -1,26 +1,23 @@
-import { FlatList, View } from "react-native";
-import { UserType } from "../../types/users";
-import UserTile from "./UserTile";
-import UsersClass from "../../models/UserModel";
-import ToggleMe from "./ToggleMe";
+import {FlatList, View} from 'react-native';
+import {UserType} from '../../types/users';
+import UserTile from './UserTile';
+import UsersClass from '../../models/UserModel';
+import ToggleMe from './ToggleMe';
 
 const UsersIndex = () => {
-    const users = UsersClass.getUsers();
+  const users = UsersClass.getUsers();
 
+  const renderItem = ({item}: {item: UserType}) => <UserTile user={item} />;
 
-    const renderItem = ({ item }: { item: UserType }) => <UserTile user={item}/>;
-
-   
-    return (
-        <View>
-            <FlatList
-                data={users}
-                renderItem={renderItem}
-                ListFooterComponent={<ToggleMe/>}
-            />
-
-        </View>
-    );
-}
+  return (
+    <View>
+      <FlatList
+        data={users}
+        renderItem={renderItem}
+        ListFooterComponent={<ToggleMe />}
+      />
+    </View>
+  );
+};
 
 export default UsersIndex;
