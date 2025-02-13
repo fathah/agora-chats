130ca1623e95cbaa38b034eb1de763e927ac633b
Fathah KA <48355244+fathah@users.noreply.github.com>
2025-02-13 18:06:19 +0400

    Crop

--- src/screens/CropVideo/crop.ts
+++ src/screens/CropVideo/crop.ts
@@ -1,49 +1,53 @@
 import RNFS from 'react-native-fs';
-import {FFmpegKit, ReturnCode} from "ffmpeg-kit-react-native";
-import {CropBox} from "./utils.ts";
-import {MutableRefObject} from "react";
-
+import {FFmpegKit, ReturnCode} from 'ffmpeg-kit-react-native';
+import {CropBox} from './utils.ts';
+import {MutableRefObject} from 'react';
 
 type CropResponse = {
-    success: boolean;
-    path: string | null;
+  success: boolean;
+  path: string | null;
 };
 
-export const cropVideo =
-    async (videoUri:string, fileName: string, videoWidth:number, videoHeight:number, width: number, height: number, cropBox: MutableRefObject<CropBox>)
-       : Promise<CropResponse> => {
-        const tempDir = RNFS.TemporaryDirectoryPath; // Path to the temp folder
-        const fileExt = fileName.split('.').pop();
-        const finalName = `${Date.now()}.${fileExt}`;
-        const outputPath = `${tempDir}cropped_${finalName}`;
+export const cropVideo = async (
+  videoUri: string,
+  fileName: string,
+  videoWidth: number,
+  videoHeight: number,
+  width: number,
+  height: number,
+  cropBox: MutableRefObject<CropBox>,
+): Promise<CropResponse> => {
+  const tempDir = RNFS.TemporaryDirectoryPath; // Path to the temp folder
+  const fileExt = fileName.split('.').pop();
+  const finalName = `${Date.now()}.${fileExt}`;
+  const outputPath = `${tempDir}cropped_${finalName}`;
 
-    const scaleX = videoWidth / width; // Width scale factor
-    const scaleY = videoHeight / height; // Height scale factor
+  const scaleX = videoWidth / width; // Width scale factor
+  const scaleY = videoHeight / height; // Height scale factor
 
-    // Convert cropBox dimensions to video-relative
-    const cropWidth = cropBox.current.width * scaleX;
-    const cropHeight = cropBox.current.height * scaleY;
-    const offsetX = cropBox.current.x * scaleX;
-    const offsetY = cropBox.current.y * scaleY;
+  // Convert cropBox dimensions to video-relative
+  const cropWidth = cropBox.current.width * scaleX;
+  const cropHeight = cropBox.current.height * scaleY;
+  const offsetX = cropBox.current.x * scaleX;
+  const offsetY = cropBox.current.y * scaleY;
 
-        console.log("CropVideo", cropWidth, cropHeight, offsetX, offsetY);
+  console.log('CropVideo', cropWidth, cropHeight, offsetX, offsetY);
 
-    const ffmpegCommand = `-i ${videoUri} -vf "crop=${cropWidth}:${cropHeight}:${offsetX}:${offsetY}" -c:a copy ${outputPath}`;
+  const ffmpegCommand = `-i ${videoUri} -vf "crop=${cropWidth}:${cropHeight}:${offsetX}:${offsetY}" -c:a copy ${outputPath}`;
 
-    try {
-        const session = await FFmpegKit.execute(ffmpegCommand);
-        const returnCode = await session.getReturnCode();
+  try {
+    const session = await FFmpegKit.execute(ffmpegCommand);
+    const returnCode = await session.getReturnCode();
 
-        return {
-            success:ReturnCode.isSuccess(returnCode),
-            path: outputPath,
-        };
-
-    } catch (e) {
-        console.log(e);
-        return {
-            success: false,
-            path:null,
-        };
-    }
+    return {
+      success: ReturnCode.isSuccess(returnCode),
+      path: outputPath,
+    };
+  } catch (e) {
+    console.log(e);
+    return {
+      success: false,
+      path: null,
+    };
+  }
 };
--- src/screens/CropVideo/index.tsx
+++ src/screens/CropVideo/index.tsx
@@ -1,266 +1,130 @@
+import 'react-native-gesture-handler';
+import React, {useMemo} from 'react';
+import { StyleSheet, View } from 'react-native';
+import Animated, {
+    useAnimatedStyle,
+    useSharedValue,
+    withSpring,
+    withTiming,
+} from 'react-native-reanimated';
 import {
-  ActivityIndicator,
-  Alert,
-  Animated,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {useNavigation} from "@react-navigation/native";
-import {useCallback, useEffect, useMemo, useRef, useState} from "react";
+    Gesture,
+    GestureDetector,
+    GestureHandlerRootView,
+} from 'react-native-gesture-handler';
 import Video from "react-native-video";
-import {
-    GestureDetector, Gesture, GestureHandlerRootView
-} from "react-native-gesture-handler";
-import {calculateContainerDimensions, CropBox} from "./utils.ts";
-import {cropVideo} from "./crop.ts";
-import {useAnimatedStyle, useDerivedValue, useSharedValue} from "react-native-reanimated";
-
-
-
-
-function CropVideo() {
-
-    const navigation = useNavigation();
-
-    const navProps = navigation.getState()!.routes[2].params;
-const videoUri = navProps?.video;
-const videoHeight = navProps?.height;
-const videoWidth = navProps?.width;
-const fileName = navProps?.fileName;
+import {useNavigation, useRoute} from "@react-navigation/native";
+import {calculateContainerDimensions} from "./utils.ts";
 
 
 
 
 
-    const {width, height} = useMemo(()=>calculateContainerDimensions(videoWidth, videoHeight), [videoWidth, videoHeight]);
+export default function CropVideo() {
 
-    const initialCropBox = useMemo(()=>({x: 0, y: 0, width, height}), [width, height]);
-
-    const [cropping, setCropping] = useState(false);
-
-
-
-    const cropBoxRef = useRef<CropBox>(initialCropBox);
-
-
-const cropBox = useSharedValue(initialCropBox);
+    const route  = useRoute();
+    const {video, width, height, fileName} = route.params;
 
 
 
-const cropBoxStyle = useDerivedValue(() => ({
-    left: cropBox.value.x,
-    top: cropBox.value.y,
-    width: cropBox.value.width,
-    height: cropBox.value.height,
-}),[]);
+    const pressed = useSharedValue<boolean>(false);
 
-
-    // Gesture for moving the crop box
-    const moveGesture = Gesture.Pan()
-        .onUpdate((event) => {
-            console.log("Moving====>");
-             const newX = Math.max(0, Math.min(width - cropBox.value.width, cropBox.value.x + event.translationX));
-             const newY = Math.max(0, Math.min(height - cropBox.value.height, cropBox.value.y + event.translationY));
-            cropBox.value = {...cropBox.value, x: newX, y: newY};
-        });
-
-    // Gesture for resizing (can be extended for each handle)
-    const resizeGesture = Gesture.Pan()
-        .onUpdate((event) => {
-            console.log("Updating+++");
-            const newWidth = Math.max(50, Math.min(width - cropBox.value.x, cropBox.value.width + event.translationX));
-            const newHeight =  Math.max(50, Math.min(height - cropBox.value.y, cropBox.value.height + event.translationY));
-            console.log("Before Handling Resize", cropBox.value);
-            cropBox.value = {...cropBox.value, width: newWidth, height: newHeight};
-            console.log("After Handling Resize", cropBox.value);
-        });
+    const offset = useSharedValue({x:0, y:0});
+    const size = useSharedValue({width: containerWidth, height: containerHeight});
+    const start = useSharedValue(({ x:0, y: 0}));
 
 
 
+    const relativeVal = useMemo(()=> calculateContainerDimensions(width, height),[width, height]);
+    const containerWidth = relativeVal.width;
+    const containerHeight = relativeVal.height;
 
-    // const handleGestureEvent = useCallback((event: GestureEvent<PanGestureHandlerEventPayload>) => {
-    //     const current = cropBoxRef.current;
-    //     const newCropBox =  scaleDimensionOnGesture(event, width, height, current);
-    //     cropBoxRef.current = newCropBox;
-    //     setCropBox({...newCropBox});
-    // },[height, width]);
-    //
-    //
-    // const onResize = useCallback((event:  GestureEvent<PanGestureHandlerEventPayload>, direction: string) => {
-    //     const current = cropBoxRef.current;
-    //     const newValue = handleResizeGesture(event, direction, current, width, height);
-    //     cropBoxRef.current = newValue;
-    //     setCropBox({...newValue});
-    // }, [width, height]);
+    const animatedStyles = useAnimatedStyle(() => ({
+        transform: [
+            { translateX: offset.value.x},
+            { translateY: offset.value.y},
+            // { scale: withTiming(pressed.value ? 1.2 : 1) },
+        ],
+        height: containerHeight,
+        width: containerWidth,
+    }));
 
 
-const onCropClick = useCallback(()=>{
-
-    setCropping(true);
-    cropVideo(videoUri, fileName, videoWidth, videoHeight, width, height, cropBoxRef).then((resp)=>{
-        setCropping(false);
-        if(resp.success){
-            console.log("New Path==>", resp.path);
-            navigation.navigate('ViewVideo', {video: resp.path});
-        }else{
-            Alert.alert("Crop Failed", "Failed to crop video");
-        }
-
+    const scale = Gesture.Pinch().onUpdate((event)=>{
+        size.value = {
+            width: size.value.width * event.scale,
+            height: size.value.height * event.scale,
+        };
     });
-},[navigation,fileName, height, videoHeight, videoUri, videoWidth, width]);
+
+    const pan = Gesture.Pan()
+        .onBegin(() => {
+            pressed.value = true;
+        })
+        .onChange((event) => {
+            const newX = Math.min(
+                Math.max(0, start.value.x + event.translationX),
+                0
+            );
+
+            const newY = Math.min(
+                Math.max(0, start.value.y + event.translationY),
+                0
+            );
+
+            // Update offset with calculated values
+            offset.value = { x: newX, y: newY };
 
 
-if(!videoUri){
-    return  <View>
-        <Text>Video Not Found</Text>
-    </View>;
-}
+        })
+        .onEnd(() => {
+            start.value = {x: offset.value.x, y: offset.value.y};
+        })
+    .onFinalize(() => {
+        // offset.value = withSpring({
+        //     x: 0,
+        //     y: 0,
+        // });
+        pressed.value = false;
+    });
 
+    const concat = Gesture.Simultaneous(pan, scale);
 
     return (
-            <GestureHandlerRootView style={styles.rootContainer}>
-
-                <GestureDetector gesture={moveGesture}>
-                    {/* Video Player */}
-                  <View style={{width, height}}>
-                      <Video
-                          source={{ uri: videoUri }}
-                          style={styles.video}
-                          resizeMode="contain"
-                          repeat
-                      />
-                      <Animated.Text style={{position:'absolute', fontSize:30}}>Value is{cropBoxStyle.value.width}</Animated.Text>
-                      {/*<Animated.View style={[styles.cropBox, cropBoxStyle]} />*/}
-
-                      <GestureDetector gesture={resizeGesture}><View style={[styles.resizeHandle, styles.topLeftHandle]} /></GestureDetector>
-                      <GestureDetector gesture={resizeGesture}><View style={[styles.resizeHandle, styles.topRightHandle]} /></GestureDetector>
-                      <GestureDetector gesture={resizeGesture}><View style={[styles.resizeHandle, styles.bottomLeftHandle]} /></GestureDetector>
-                      <GestureDetector gesture={resizeGesture}><View style={[styles.resizeHandle, styles.bottomRightHandle]} /></GestureDetector>
-
-
-
-
-                  </View>
-
-
-                    {/*<View style={[styles.overlay, {top: 0, width: width, height: cropBox.y}]}/>*/}
-                    {/*<View style={[styles.overlay, {bottom: 0, width:width, height: height - cropBox.y - cropBox.height}]}/>*/}
-                    {/*<View style={[styles.overlay, {left: 0, height: height, width: cropBox.x}]}/>*/}
-                    {/*<View style={[styles.overlay, {right: 0, height: height, width: width - cropBox.x - cropBox.width}]}/>*/}
-
-                    {/* Crop Overlay */}
-                    {/*<PanGestureHandler onGestureEvent={handleGestureEvent}>*/}
-                    {/*    <View*/}
-                    {/*        style={[*/}
-                    {/*            styles.cropBox,*/}
-                    {/*            {*/}
-                    {/*                width: cropBox.width,*/}
-                    {/*                height: cropBox.height,*/}
-                    {/*                left: cropBox.x,*/}
-                    {/*                top: cropBox.y,*/}
-                    {/*            },*/}
-                    {/*        ]}*/}
-                    {/*    >*/}
-                    {/*        <PanGestureHandler*/}
-                    {/*            onGestureEvent={(event) => onResize(event, "top-left")}>*/}
-                    {/*            <View style={[styles.resizeHandle, styles.topLeftHandle]} />*/}
-                    {/*        </PanGestureHandler>*/}
-
-                    {/*        <PanGestureHandler*/}
-                    {/*            onGestureEvent={(event) => onResize(event, "top-right")}>*/}
-                    {/*            <View style={[styles.resizeHandle, styles.topRightHandle]} />*/}
-                    {/*        </PanGestureHandler>*/}
-
-                    {/*        <PanGestureHandler*/}
-                    {/*            onGestureEvent={(event) => onResize(event, "bottom-left")}>*/}
-                    {/*            <View style={[styles.resizeHandle, styles.bottomLeftHandle]} />*/}
-                    {/*        </PanGestureHandler>*/}
-
-                    {/*        <PanGestureHandler*/}
-                    {/*            onGestureEvent={(event) => onResize(event, "bottom-right")}>*/}
-                    {/*            <View style={[styles.resizeHandle, styles.bottomRightHandle]} />*/}
-                    {/*        </PanGestureHandler>*/}
-
-                    {/*    </View>*/}
-                    {/*</PanGestureHandler>*/}
-
-
+        <GestureHandlerRootView style={styles.container}>
+            <View style={[styles.container,{ width:containerWidth,
+                height:containerHeight}]}>
+                <Video
+                source={{
+                    uri: video
+                }}
+                style={{
+                   width:"100%",
+                   height:"100%",
+                    position:"absolute"
+                }}
+                />
+                <GestureDetector gesture={concat}>
+                    <Animated.View style={[styles.cropBox, animatedStyles]} />
                 </GestureDetector>
-
-
-                <TouchableOpacity style={styles.cropButton} onPress={onCropClick}>
-                    {cropping &&  <ActivityIndicator size="small" color="#000" />}
-                    <Text>{cropping ? 'Cropping...' : 'Crop It'}</Text>
-                </TouchableOpacity>
-            </GestureHandlerRootView>
-
+            </View>
+        </GestureHandlerRootView>
     );
 }
 
-export default CropVideo;
-
 const styles = StyleSheet.create({
-    rootContainer:{
-        flex:1,
-        position:'relative',
-        backgroundColor: "black",
-        justifyContent: "center",
-        alignItems: "center",
-    },
     container: {
-        backgroundColor: "black",
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'relative',
 
     },
-    video: {
- position: "absolute", top: 0, left: 0, width: "100%", height: "100%"
-    },
-    overlay: {
-        position: "absolute",
-        backgroundColor: "rgba(0, 0, 0, 0.9)",
-        filter:'blur(0.5px)',
-    },
-
     cropBox: {
-        position: "absolute",
+        transformOrigin:'center',
+        borderColor: '#b58df1',
         borderWidth: 5,
-        borderColor: "rgba(255,255,255,0.5)",
+        position: 'absolute',
 
-    },
-    resizeHandle: {
-        position: "absolute",
-        width: 20,
-        height: 20,
-        backgroundColor: "rgba(255,255,255,0.5)",
-        borderRadius: 10,
-    },
-    topLeftHandle: { top: -10, left: -10 },
-    topRightHandle: { top: -10, right: -10 },
-    bottomLeftHandle: { bottom: -10, left: -10 },
-    bottomRightHandle: { bottom: -10, right: -10 },
-
-    resizeEdge: {
-        position: "absolute",
-        backgroundColor: "rgba(255,255,255,0.3)",
-    },
-    topEdge: { top: -5, left: 10, right: 10, height: 10 },
-    rightEdge: { top: 10, right: -5, bottom: 10, width: 10 },
-    bottomEdge: { left: 10, right: 10, bottom: -5, height: 10 },
-    leftEdge: { top: 10, bottom: 10, left: -5, width: 10 },
-
-    cropButton:{
-
-        position:'absolute',
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'center',
-        gap:10,
-        bottom:30,
-        right:20,
-        backgroundColor: 'white',
-        paddingHorizontal:25,
-        paddingVertical:15,
-        borderRadius:8,
     },
 });
